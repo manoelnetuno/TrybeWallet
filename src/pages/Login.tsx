dde783dd57253b02f84788a57aae7d7a0cd6b426
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-02-05 10:17:26 -0300

    primeiro commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
-  return <div>Login</div>;
+  const [login, setlogin] = useState('');
+  const [password, setpassword] = useState('');
+  const navigate = useNavigate();
+  const emailvalidate = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const passwordminimum = password.length < 6;
+
+  return (
+    <div>
+      <form
+        onSubmit={ (event) => {
+          event.preventDefault();
+          navigate('/carteira');
+        } }
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        data-testid="email-input"
+        type="email"
+        placeholder="Seu email"
+        onChange={ (event) => setlogin(event.target.value) }
+        value={ login }
+        required
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        onChange={ (event) => setpassword(event.target.value) }
+        value={ password }
+        required
+      />
+      <button type="submit" disabled={ passwordminimum || !emailvalidate }>
+        Entrar
+      </button>
+    </div>
+  );
 }
 
 export default Login;
