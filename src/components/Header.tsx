19ea5b7e8c494ec71d1e0fc90146ca14930445a9
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-02-06 13:45:00 -0300

    feat:atualizando codigo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
+import { useSelector } from 'react-redux';
+import { GlobalState } from '../types/types';
+
 function Header() {
-  return <div>Header</div>;
+  const DataUser = useSelector((state: GlobalState) => state.user.email);
+  return (
+    <div>
+      <p data-testid="email-field">{DataUser}</p>
+      <p data-testid="total-field">0</p>
+      <p data-testid="header-currency-field">BRL</p>
+    </div>
+
+  );
 }
 
 export default Header;
--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -1,1 +1,25 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+
+import { AnyAction } from 'redux';
+import { LOGIN_DATA } from '../actions';
+
+type UserState = {
+  email: string,
+};
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+function userData(state = INITIAL_STATE, action: AnyAction): UserState {
+  switch (action.type) {
+    case LOGIN_DATA:
+      return {
+        ...state,
+        email: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export default userData;
